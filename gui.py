8eca410fa78a7bfbfbabbeaa02c3075ecf2bc4d8
CodX48 <codx207@gmail.com>
2025-07-22 11:48:43 +0300

    right side is smaller

--- gui.py
+++ gui.py
@@ -44,9 +44,11 @@ class WaterMeterGUI(QMainWindow):
 
         self.tab_widget = QTabWidget()
 
-        # Swap the order here
         self.main_layout.addWidget(self.tab_widget)
         self.main_layout.addWidget(self.right_container)
+        
+        self.right_container.setFixedWidth(300)
+
 
         self.home_tab = QWidget()
         self.advanced_tab = QWidget()
